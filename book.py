45503b1e3bdd1cf4a7a8afe2bcc4138e81a1147f
balancy <balancy@gmail.com>
2021-03-17 18:58:42 +0100

    Create Book class and implement all methods inside class

---
+++ book.py
@@ -0,0 +1,120 @@
+import os
+import re
+
+from bs4 import BeautifulSoup
+from pathvalidate import sanitize_filename
+import requests
+
+
+class Book:
+    def __init__(self, site_url="https://tululu.org/", book_id=1, image_folder='images/', books_folder='books/'):
+        self.site_url = site_url
+        self.book_id = book_id
+        self.image_folder = image_folder
+        self.books_folder = books_folder
+
+    def create_dirs(self) -> None:
+        """Checks if folders exist. If not, create them.
+        """
+
+        for folder in (self.books_folder, self.image_folder):
+            if not os.path.exists(folder):
+                os.makedirs(folder)
+
+    def get_html(self) -> str:
+        """Gets html code for book page.
+
+        :return: html code
+        """
+
+        r = requests.get(f"{self.site_url}/b{str(self.book_id)}/", verify=False, allow_redirects=False)
+        r.raise_for_status()
+
+        if r.status_code == 302:
+            raise requests.HTTPError
+
+        return r.text
+
+    @staticmethod
+    def get_title_image(html) -> (str, str):
+        """Gets book title and image link from html code.
+
+        :param html: html code
+        :return: book title and image link
+        """
+
+        soup = BeautifulSoup(html, "lxml")
+        title_author = soup.find('table', class_='tabs').find('div', id='content').find('h1').text
+        title, author = title_author.split('::')
+        image_url = soup.find('table', class_='d_book').find('img')['src']
+
+        return f"{sanitize_filename(title.strip())}", image_url
+
+    def download_image(self, image_url) -> str:
+        """Saves image in given folder.
+
+        :param image_url: link where to get image
+        :return: path to the image on the drive
+        """
+
+        response = requests.get(f"{self.site_url}{image_url}", verify=False)
+        response.raise_for_status()
+
+        image_name = image_url.split('/')[-1]
+        path = f"{self.image_folder}{image_name}"
+
+        with open(path, 'wb') as f:
+            f.write(response.content)
+
+        return path
+
+    @staticmethod
+    def get_txt_link(html) -> str:
+        """Getting relative link to download txt.
+
+        :param html: html code
+        :return: relative link to download txt
+        """
+
+        soup = BeautifulSoup(html, 'lxml')
+        link_to_txt = soup.find('a', title=re.compile('скачать книгу txt'))
+        if not link_to_txt:
+            raise FileNotFoundError
+
+        return link_to_txt['href']
+
+    def download_txt(self, text_url, title) -> str:
+        """Downloads txt file and saves it in given folder.
+
+        :param text_url: link where to get file
+        :param title: title of the book
+        :return: path to the file
+        """
+
+        response = requests.get(f"{self.site_url}{text_url}", verify=False)
+        response.raise_for_status()
+
+        path = f"{self.books_folder}{title}.txt"
+
+        with open(path, 'wb') as f:
+            f.write(response.content)
+
+        return path
+
+    @staticmethod
+    def get_comments(html) -> list:
+        """Gets comments from html code.
+
+        :param html: html code
+        :return: comments
+        """
+
+        soup = BeautifulSoup(html, 'lxml')
+        comment_elms = soup.find_all('div', class_='texts')
+
+        comments = list()
+        if comment_elms:
+            for comment_elm in comment_elms:
+                comments.append(comment_elm.find('span').text)
+
+        return comments
--- main.py
+++ main.py
@@ -1,156 +1,28 @@
-import os
-import re
-
-from bs4 import BeautifulSoup
-from pathvalidate import sanitize_filename
 import requests
 
+from book import Book
 
 BOOKS_FOLDER = "books/"
 IMAGES_FOLDER = "images/"
 SITE_URL = "https://tululu.org/"
 NUMBER_BOOKS = 10
 
-
-def check_for_redirect(status_code) -> None:
-    """Checks if given status_code is redirect status code.
-
-    :param status_code: given status_code
-    """
-
-    if status_code == 302:
-        raise requests.HTTPError
-
-
-def get_html(link, book_id) -> str:
-    """Gets html code from given link.
-
-    :param link: site link
-    :param book_id: id of the book on site
-    :return: html code
-    """
-
-    r = requests.get(f"{link}/b{book_id}/", verify=False, allow_redirects=False)
-    r.raise_for_status()
-    check_for_redirect(r.status_code)
-
-    return r.text
-
-
-def get_title_image(html) -> (str, str):
-    """Gets book title and image link from html.
-
-    :param html: html code
-    :return: book title and image link
-    """
-
-    soup = BeautifulSoup(html, "lxml")
-    title_author = soup.find('table', class_='tabs').find('div', id='content').find('h1').text
-    title, author = title_author.split('::')
-    image_url = soup.find('table', class_='d_book').find('img')['src']
-
-    return f"{sanitize_filename(title.strip())}", image_url
-
-
-def create_dir(path) -> None:
-    """Checks if directory exists. If not, create it.
-
-    :param path: directory path
-    """
-
-    if not os.path.exists(path):
-        os.makedirs(path)
-
-
-def get_txt_link(html) -> str:
-    """Getting relative link to download txt.
-
-    :param html: html code
-    :return: relative link to download txt
-    """
-
-    soup = BeautifulSoup(html, 'lxml')
-    link_to_txt = soup.find('a', title=re.compile('скачать книгу txt'))
-    if not link_to_txt:
-        raise FileNotFoundError
-
-    return link_to_txt['href']
-
-
-def download_txt(url, filename, folder='books/') -> str:
-    """Downloads txt file and saves it in given folder.
-
-    :param url: link where to get file
-    :param filename: name of file to save
-    :param folder: folder where to save file
-    :return: path to the file
-    """
-
-    create_dir(folder)
-    path = f"{folder}{sanitize_filename(filename)}"
-
-    response = requests.get(f"{SITE_URL}{url}", verify=False)
-    response.raise_for_status()
-
-    with open(path, 'wb') as f:
-        f.write(response.content)
-
-    return path
-
-
-def download_image(url, folder='images/') -> str:
-    """Saves image in given folder.
-
-    :param url: link where to get image
-    :param folder: folder where to save image
-    :return: path to the image
-    """
-
-    create_dir(folder)
-
-    response = requests.get(f"{SITE_URL}{url}", verify=False)
-    response.raise_for_status()
-
-    image_name = url.split('/')[-1]
-    path = f"{folder}{image_name}"
-
-    with open(path, 'wb') as f:
-        f.write(response.content)
-
-    return path
-
-
-def get_comments(html) -> list:
-    """Gets comments from html code.
-
-    :param html: html code
-    :return: comments
-    """
-
-    soup = BeautifulSoup(html, 'lxml')
-    comment_elms = soup.find_all('div', class_='texts')
-
-    comments = list()
-    if comment_elms:
-        for comment_elm in comment_elms:
-            comments.append(comment_elm.find('span').text)
-
-    return comments
-
-
 if __name__ == "__main__":
     requests.packages.urllib3.disable_warnings()
 
     for book_id in range(1, NUMBER_BOOKS):
         try:
-            html = get_html(SITE_URL, book_id)
-            title, image_url = get_title_image(html)
-            path_image = download_image(image_url, IMAGES_FOLDER)
-            relative_txt_link = get_txt_link(html)
-            path_book = download_txt(relative_txt_link, f"{title}.txt", BOOKS_FOLDER)
-            comments = get_comments(html)
-            if comments:
-                print(comments)
+            book = Book(books_folder=BOOKS_FOLDER, image_folder=IMAGES_FOLDER, site_url=SITE_URL, book_id=book_id)
+            book.create_dirs()
+            html = book.get_html()
+
+            title, image_url = book.get_title_image(html)
+            path_image_on_disc = book.download_image(image_url)
+
+            relative_txt_url = book.get_txt_link(html)
+            path_txt_on_disc = book.download_txt(relative_txt_url, title)
+
+            comments = book.get_comments(html)
         except requests.HTTPError:
             print(f"For book with id={book_id} there is a redirection")
         except FileNotFoundError:
